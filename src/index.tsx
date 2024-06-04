2e3b698b1b5d94f5e475e33c756b89c34a834bf0
xiangshangzhi <xiangshangzhi@163.com>
2024-06-04 17:23:11 +0800

    feat: init project

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+/*
+ * @Author: xiangshangzhi xiangshangzhi@xtfer.com
+ * @Date: 2022-07-20 12:55:04
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2024-06-04 17:17:45
+ * @FilePath: \react-base\src\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// webpack的入口文件：src/index.tsx
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.less';
+
+function App() {
+  return (
+    <div>
+      <h1>hello , react!</h1>
+    </div>
+  );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
