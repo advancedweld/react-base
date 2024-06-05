d070f90047dac06c57ea7720d2911428d7bf7b74
xiangshangzhi <xiangshangzhi@163.com>
2024-06-05 11:48:26 +0800

    feat: ts支持/babel配置

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@
  * @Author: xiangshangzhi xiangshangzhi@xtfer.com
  * @Date: 2022-07-20 12:55:04
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-06-04 17:53:43
+ * @LastEditTime: 2024-06-05 10:48:57
  * @FilePath: \react-base\src\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // webpack的入口文件：src/index.tsx
 
-import React from 'react';
+// import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import './style.less';
